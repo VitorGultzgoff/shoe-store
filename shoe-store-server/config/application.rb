59169bc1b742aba3016da5ed9340b3b1a8b26c9f
Vitor Gultzgoff <vlgultzgoff@gmail.com>
2022-08-21 11:02:29 -0300

    chore: configurations applied to use graphiql query client in development

--- shoe-store-server/config/application.rb
+++ shoe-store-server/config/application.rb
@@ -1,4 +1,5 @@
 require_relative "boot"
+require "sprockets/railtie"
 
 require "rails/all"
 
@@ -23,6 +24,8 @@ module ShoeStoreServer
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore, key: '_namespace_key'
     config.active_job.queue_adapter = :sidekiq
   end
 end
