21b7776e8d03e2f19d177d237d0495809417b575
Vitor Gultzgoff <vlgultzgoff@gmail.com>
2022-08-20 21:54:30 -0300

    feat: initial graphql setup

--- shoe-store-server/config/routes.rb
+++ shoe-store-server/config/routes.rb
@@ -5,6 +5,10 @@ Sidekiq::Web.use ActionDispatch::Cookies
 Sidekiq::Web.use ActionDispatch::Session::CookieStore, key: "_interslice_session"
 
 Rails.application.routes.draw do
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+  end
+  post "/graphql", to: "graphql#execute"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
