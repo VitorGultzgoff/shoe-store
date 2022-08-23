18f4f4c66044f9dcd6b9e28dcbf2b6539214ad8a
Vitor Gultzgoff <vlgultzgoff@gmail.com>
2022-08-23 12:45:15 -0300

    chore: remove notifications icon from navbar

--- shoe-store-client/src/layouts/ApplicationLayout/components/ApplicationNavbar.tsx
+++ shoe-store-client/src/layouts/ApplicationLayout/components/ApplicationNavbar.tsx
@@ -52,14 +52,6 @@ export const ApplicationNavbar = ({ onSidebarOpen }) => {
           >
             <MenuIcon fontSize="small" />
           </IconButton>
-          <Box sx={{ flexGrow: 1 }} />
-          <Tooltip title="Notifications">
-            <IconButton sx={{ ml: 1 }}>
-              <Badge badgeContent={4} color="primary" variant="dot">
-                <BellIcon fontSize="small" />
-              </Badge>
-            </IconButton>
-          </Tooltip>
         </Toolbar>
       </ApplicationNavbarRoot>
     </>
