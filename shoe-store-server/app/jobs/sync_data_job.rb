d7629ac8becd4d96a8e625e8b390c9bd25bc931c
Vitor Gultzgoff <vitor@fyclabs.com>
2022-08-23 23:33:34 -0300

    chore: product suggestions job moved into the main job

--- shoe-store-server/app/jobs/sync_data_job.rb
+++ shoe-store-server/app/jobs/sync_data_job.rb
@@ -18,18 +18,39 @@ class SyncDataJob < ApplicationJob
 
           store_created = Store.find_or_create_by(name: store_name)
           product_created = Product.find_or_create_by(name: product_name)
+          Sale.create(store_id: store_created.id, product_id: product_created.id, amount: inventory_amount)
           
-          product_created_id = product_created["id"]
-
-          store_created_id = store_created["id"]
-          inventory_is_created = ProductInventory.find_by(store_id: store_created_id, product_id: product_created_id)
-          if inventory_is_created
-            inventory_is_created.amount = inventory_amount
-            inventory_is_created.save
-          else
-            ProductInventory.create(store_id: store_created_id, product_id: product_created_id, amount: inventory_amount)
+          inventory_saved = ProductInventory.find_or_create_by(store_id: store_created.id, product_id: product_created.id)
+          if inventory_saved != inventory_amount
+            inventory_saved.amount = inventory_amount
+            inventory_saved.save
           end
-          Sale.create(store_id: store_created_id, product_id: product_created_id, amount: inventory_amount)
+
+          # Include new suggestions
+          products_with_low_inventory = ProductInventory.where("amount <= 30")
+          products_with_high_inventory = ProductInventory.where("amount > 70")
+          if products_with_low_inventory && products_with_high_inventory
+            for actual_product_with_low_inventory in products_with_low_inventory
+              for actual_product_with_high_inventory in products_with_high_inventory
+                target_product_name = Product.find(actual_product_with_low_inventory["product_id"])["name"]
+                source_product_name = Product.find(actual_product_with_high_inventory["product_id"])["name"]    
+                if target_product_name == source_product_name
+                  ProductInventorySuggestion.find_or_create_by(inv_sugg_src_id: actual_product_with_high_inventory["id"], inv_sugg_target_id: actual_product_with_low_inventory["id"])
+                end
+              end
+            end
+          end
+
+          # Sync the suggestions according to the updated inventories
+          all_suggestions = ProductInventorySuggestion.all
+          for actual_suggestion in all_suggestions
+            actual_suggestion_source_amount = ProductInventory.find(actual_suggestion["inv_sugg_src_id"])["amount"]
+            actual_suggestion_target_amount = ProductInventory.find(actual_suggestion["inv_sugg_target_id"])["amount"]
+            if actual_suggestion_target_amount > 30 || actual_suggestion_source_amount <=70
+              actual_suggestion.destroy
+            end
+          end
+
         end
       }
     end
--- shoe-store-server/app/models/product_inventory.rb
+++ shoe-store-server/app/models/product_inventory.rb
@@ -1,10 +1,5 @@
 class ProductInventory < ApplicationRecord
     belongs_to :product
     belongs_to :store
-
-    after_save :retrieve_suggestions
-    
-    def retrieve_suggestions
-        InventorySuggestionsJob.set(wait: 3.minute).perform_later
-    end
+    has_many :product_inventory_suggestions
 end
